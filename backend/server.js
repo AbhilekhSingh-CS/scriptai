df33012d01d70aae1135ea57fcbce5fe7f17db04
AbhilekhSingh-CS <abhilekhsinghyadav1@gmail.com>
2026-08-16 03:09:38 +0530

    Restrict CORS to allowed origins

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+  'http://localhost:5173',
+  process.env.FRONTEND_URL,
+].filter(Boolean);
+
+app.use(cors({ origin: allowedOrigins }));
 app.use(express.json());
 
 app.use('/api/script', require('./routes/scriptRoutes'));
